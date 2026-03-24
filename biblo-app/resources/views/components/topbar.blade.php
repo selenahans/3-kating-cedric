04379ca92c3516ce272b8e2310b77a2fa7ded62a
selena <selenahans49@gmail.com>
2026-03-24 09:31:54 +0700

    navbar atas bisa ke halaman notifications

--- biblo-app/resources/views/components/topbar.blade.php
+++ biblo-app/resources/views/components/topbar.blade.php
@@ -11,10 +11,12 @@
     </div>
 
     <div class="flex items-center gap-3">
-        <button class="icon-btn group">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path><path d="M13.73 21a2 2 0 0 1-3.46 0"></path></svg>
-            <span class="notification-dot"></span>
-        </button>
+        <a href="{{ url('/notification') }}" class="inline-flex">
+            <button class="icon-btn group">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path><path d="M13.73 21a2 2 0 0 1-3.46 0"></path></svg>
+                <span class="notification-dot"></span>
+            </button>
+        </a>
 
         <div class="user-nav-profile">
             <div class="text-right hidden sm:block">
